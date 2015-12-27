e1348c9024ab41f0aa9f33dca50b44af4f3c082d
shiraji <isogai.shiraji@gmail.com>
2015-12-28 08:25:24 +0900

    temporary commit for gens

---
+++ sample/src/main/java/permissions/dispatcher/sample/MainFragment2.java
@@ -0,0 +1,36 @@
+package permissions.dispatcher.sample;
+
+import android.Manifest;
+import android.app.Fragment;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class MainFragment2 extends Fragment {
+    @NeedsPermission(Manifest.permission.INTERNET)
+    void internet() {
+    }
+
+    @OnShowRationale(Manifest.permission.INTERNET)
+    void internetOnShow(PermissionRequest request) {
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_SETTINGS)
+    void writeSettings() {
+    }
+
+    @OnShowRationale(Manifest.permission.WRITE_SETTINGS)
+    void writeSettingOnShowRationale(PermissionRequest request) {
+    }
+
+    @NeedsPermission(Manifest.permission.SYSTEM_ALERT_WINDOW)
+    void showSystemAlertWindowPermission() {
+    }
+
+    @OnShowRationale(Manifest.permission.SYSTEM_ALERT_WINDOW)
+    void showSystemAlertWindowRationale(PermissionRequest request) {
+    }
+}
