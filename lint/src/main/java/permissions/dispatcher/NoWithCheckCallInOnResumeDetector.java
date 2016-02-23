14ed72bc860fa9cef1f9d7b67bb907bb45da8115
shiraji <isogai.shiraji@gmail.com>
2016-02-23 14:26:12 +0900

    Add issue

--- lint/src/main/java/permissions/dispatcher/NoWithCheckCallInOnResumeDetector.java
+++ lint/src/main/java/permissions/dispatcher/NoWithCheckCallInOnResumeDetector.java
@@ -1,12 +1,27 @@
 package permissions.dispatcher;
 
+import com.android.tools.lint.detector.api.Category;
 import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Implementation;
+import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.Scope;
+import com.android.tools.lint.detector.api.Severity;
+
+import java.util.EnumSet;
 
 import lombok.ast.AstVisitor;
 import lombok.ast.ForwardingAstVisitor;
 
 public class NoWithCheckCallInOnResumeDetector extends Detector implements Detector.JavaScanner {
+
+    public static final Issue ISSUE = Issue.create("NoWithCheckCallInOnResume",
+            "TBD", // TODO: think these
+            "TBD",
+            Category.CORRECTNESS,
+            1,
+            Severity.WARNING,
+            new Implementation(NoWithCheckCallInOnResumeDetector.class, EnumSet.of(Scope.ALL_JAVA_FILES)));
 
     @Override
     public AstVisitor createJavaVisitor(JavaContext context) {
